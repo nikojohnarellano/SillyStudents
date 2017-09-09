54cfb858cc2d441265df868c7baccf96996ad5c4
Niko Arellano <narellano@my.bcit.ca>
2017-09-09 01:52:39 -0700

    Added code documentations

--- Student.php
+++ Student.php
@@ -1,33 +1,54 @@
-<?php 
+<?php
 
 /**
- *
- *
- */ 
+ * A class that represents a Student
+ */
 class Student {
-	
+
+	/**
+	 * Default constructor
+	 */
 	function __construct() {
 		$this->surname = '';
 		$this->first_name = '';
 		$this->emails = array();
 		$this->grades = array();
 	}
-	
+
+	/**
+	 * Adds an email to the student
+	 * @param which
+	 *			- whether the email is for home, work, etc.
+	 * @param address
+	 *			- the email address to store
+	 */
 	function add_email($which,$address) {
 		$this->emails[$which] = $address;
 	}
 
+	/**
+	 * Adds grade to the student
+	 * @param grade
+	 *			- grade to be added
+	 */
 	function add_grade($grade) {
 		$this->grades[] = $grade;
 	}
-	
+
+	/**
+	 * Method that computes and returns the average of the Student
+	 * @return average
+	 */
 	function average() {
 		$total = 0;
 		foreach ($this->grades as $value)
 			$total += $value;
 		return $total / count($this->grades);
 	}
-	
+
+	/**
+	 *	Returns the Student Info in String format
+	 */
 	function toString() {
 		$result = $this->first_name . ' ' . $this->surname;
 		$result .= ' ('.$this->average().")\n";
